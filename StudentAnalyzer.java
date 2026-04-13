f15bd620c23fd0cc8e1c3af837841f91a384015e
hello8473927 <mohammadkadiamin826@gmail.com>
2026-04-13 08:52:43 +0400

    added countAbove method  to StudentAnalyzer.java
    returns the number of students whos grades are strictly greater than the specified threshold in the form of an int.

--- StudentAnalyzer.java
+++ StudentAnalyzer.java
@@ -4,4 +4,14 @@ public class StudentAnalyzer{
   public StudentAnalyzer(ArrayList<Student> list){
     studentList = list;
   }
+
+  public int countAbove(int threshold){
+    int count = 0;
+    for(int i = 0; i < studentList.length; i++){
+      if(studentList.get(i).getMarks() > threshold){
+        count++;
+      }
+    }
+    return count;
+  }
 }
